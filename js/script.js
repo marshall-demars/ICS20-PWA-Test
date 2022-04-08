8a41e1a906d712ef4d0b2694d29462707b3255fc
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-04-08 13:15:50 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,10 @@ if (navigator.serviceWorker) {
     scope: "/ICS2O-PWA-Test/",
   })
 }
+
+/**
+ * This function displays an alert.
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+}
