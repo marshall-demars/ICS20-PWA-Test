1e93d986bea36e6d2c6835339790968267619f70
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-04-08 13:08:41 -0400

    5 new files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2022 Marshall All rights reserved
+//
+// Created by: Marshall
+// Created on: Apr 2022
+// This file contains the JS functions for index.html
+
+/**
+ * Check servie worker.
+ */
+ if (navigator.serviceWorker) {
+    navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+      scope: "/ICS2O-PWA-Test/",
+    })
+  }
+  
